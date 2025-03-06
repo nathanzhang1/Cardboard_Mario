a14d80f3f3e5c1b737da2d403a1771eef870de22
Brendan Staines <brendanstaines@gmail.com>
2025-03-06 14:57:06 -0800

    Added Mario jump animation

--- main.js
+++ main.js
@@ -34,23 +34,46 @@ document.body.appendChild(renderer.domElement);
 // controls.maxDistance = 50;
 
 //Load 1-1 Model
+
+
+
+const geometry = new THREE.BoxGeometry(1.05, 1.05, 1.05);
+const wireframe = new THREE.WireframeGeometry(geometry);
+const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.LineSegments(wireframe, material);
+cube.position.set(0, 1.5, 0.35);
+
+ 
 let level;
+let parts = {};
 const loader = new GLTFLoader();
+//scene.add(cube);
 loader.load(
-    'assets/super_mario_bros._level_1_-_1.glb', 
+    'assets/namedLevel.glb', 
     function (gltf) {
         level = gltf.scene;
-        level.position.set(65, 0, 0);
+        level.position.set(65.69, 0, 0);
+        //level.scale.set(2, 2, 2);
         scene.add(level);
+        level.traverse((child) => {
+            console.log(child.name);
+            parts[child.name] = child;
+        })
     },
     function (xhr) {
         console.log(`Loading: ${(xhr.loaded / xhr.total) * 100}% loaded`);
     },
     function (error) {
         console.error('Error loading model:', error);
-    }
+    },
+    
 );
+console.log('parts', parts);
 
+
+//const box = new THREE.Box3().setFromObject(level);
+//const boxHelper = new THREE.Box3Helper(box, 0x00ff00); 
+//scene.add(boxHelper);
 
 // //Create Player
 let player; // Declare player globally
@@ -230,12 +253,39 @@ document.addEventListener("keyup", (event) => {
     if (event.code === "Space") keys.jump = false;
 });
 
-let mapRendered = true;
-document.addEventListener("keydown", (event) => {
-    if (event.key == "`") {scene.remove(level);}
-});
+
+let state = 0;
+let appear = true;
+let stuff;
+
 document.addEventListener("keyup", (event) =>{
-    if (event.key == "`"){scene.add(level);}
+    if (event.key == "`"){
+        state ++;
+        if((state % 3) == 1){scene.add(cube);}
+        if((state % 3) == 2){scene.remove(level);}
+        if((state % 3) == 0){scene.remove(cube); scene.add(level);}
+
+        console.log("state", state % 3);
+    
+    }
+
+    if(event.key == 'p'){
+        if(appear){
+            appear = false; 
+            stuff = parts['Object_4'];
+            console.log(stuff);
+            scene.remove(stuff);
+            console.log('part removed')
+        }
+        else if (!appear){
+            appear = true; 
+            stuff = parts['Object_4'];
+            console.log(stuff);
+            stuff.scale.set(100, 100, 100);
+            scene.add(stuff);
+            console.log('part added');
+        }
+    }
 });
 
 
